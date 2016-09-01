b324290608d0449af81b5b0a3d8f556bdbb7ee5d
Kevin Micol <kevin.d.micol@gmail.com>
2016-09-01 13:02:34 -0400

    Added bootstrap, configured pods

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'product-site',
+    podModulePrefix: 'product-site/pods',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
